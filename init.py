adc20939766704b35906d19d352ade6b1284f2d4
Jens Schaa <jens.schaa@otto.de>
2014-09-18 17:20:46 +0200

    Ignore exceptions and only update when version test is succesful.

--- init.py
+++ init.py
@@ -11,8 +11,10 @@ def plugin_loaded():
 	up = Update(path, raw_version_url, repo_url)
 	try:
 		outdated = up.checkForUpdate()
-	if outdated:
-		sublime.message_dialog("There is a new version of JiraWithLime. Please wait a moment while i update your files.")
-		#up.update()
-		window = sublime.active_window()
-		window.open_file(path+'/changelog.txt')
+		if outdated:
+			sublime.message_dialog("There is a new version of JiraWithLime. Please wait a moment while i update your files.")
+			up.update()
+			window = sublime.active_window()
+			window.open_file(path+'/changelog.txt')
+	except:
+		pass
