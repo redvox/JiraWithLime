fa8c3f3790a5af65df8ef45061fba5e494b1f02b
Jens Schaa <jens.schaa@otto.de>
2014-09-18 17:22:08 +0200

    local path now get pruned with package name, instead of fixed length.

--- update.py
+++ update.py
@@ -28,27 +28,28 @@ class Update():
 
 	def update(self):
 		response = requests.get(self.repo_url+self.online_version+'.zip', stream=True)
+		print("Update response", response.status_code, self.repo_url+self.online_version+'.zip')
 		if response.status_code == 200:
 			with open(self.path+'update.zip', 'wb') as f:
 				for chunk in response.iter_content():
 					f.write(chunk)
 
-		fh = open(self.path+'update.zip', 'rb')
-		z = zipfile.ZipFile(fh)
-		print("z.namelist()", z.namelist())
-		for name in z.namelist():
-			local_name = name[17:]
-			directory = re.search(r'.*/$', name)
-			directory_requests = re.search(r'./requests/.', name)
-			print(found)
-			if name != 'JiraWithLime.sublime-settings' and local_name != '' and not directory and not directory_requests:
-				print('name', name)
-				print('local_name', local_name)
-		
-				data = z.read(name)
-		
-				myfile = open(self.path+local_name, "wb")
-				myfile.write(data)
-				myfile.close()
-				# z.extract('JiraWithLime1-0/version.txt', '')
-		fh.close()
+			fh = open(self.path+'update.zip', 'rb')
+			z = zipfile.ZipFile(fh)
+			package_name_length = len(z.namelist()[0])
+			print("z.namelist()", z.namelist())
+			for name in z.namelist():
+				local_name = name[package_name_length:]
+				directory = re.search(r'.*/$', name)
+				directory_requests = re.search(r'./requests/.', name)
+				print(found)
+				if name != 'JiraWithLime.sublime-settings' and local_name != '' and not directory and not directory_requests:
+					print('name', name)
+					print('local_name', local_name)
+			
+					data = z.read(name)
+			
+					# myfile = open(self.path+local_name, "wb")
+					# myfile.write(data)
+					# myfile.close()
+			fh.close()
